d7ce031e8d4a199dacab0b161c83ed80097151de
DoktorMandarin <faa1995alex@gmail.com>
2021-05-03 20:43:25 +0300

    Задание 1.3

---
+++ Paintings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paintings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int paintings = 52;
+            int picturesinarow;  //заполненные ряды кортин
+            int paintingsb; //картин вне ряда
+            picturesinarow = paintings / 3;
+            paintingsb = paintings % 3;
+            Console.Write("Полностью заполненных рядов " + (picturesinarow) + " и картин сверх меры " + (paintingsb) + ".");
+            Console.ReadLine();
+        }
+    }
+}
