c69bdf9a1ef49a80fb955cb30d104e3c2c7b60d7
DoktorMandarin <faa1995alex@gmail.com>
2021-05-04 23:16:56 +0300

    Задача 1.5

---
+++ 1.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int WaitingTime = 10;
+            int OldLady;
+            int hours;
+            int minutes;
+
+            Console.Write("Сколько в очереди старушек?:");
+            OldLady = Convert.ToInt32(Console.ReadLine());
+            hours = (OldLady * WaitingTime) / 60;
+            minutes = (OldLady * WaitingTime) % 60;
+            Console.Write("Вам стоять в очереди " + (hours) + " часов и " + (minutes) + " минут");
+            Console.ReadLine();
+        }
+    }
+}
