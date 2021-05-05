9d4bbca066a7006e66d7f21334118042a8424e2c
DoktorMandarin <faa1995alex@gmail.com>
2021-05-05 20:49:10 +0300

    задача 1.4

--- 1.4/Program.cs
+++ 1.4/Program.cs
@@ -18,7 +18,7 @@ namespace Score
             golds = Convert.ToInt32(Console.ReadLine());
             Console.Write("У меня кристалы по цене " + (crystalForGold) + " золота за штуку. Сколько хотите купить?");
             crystals = Convert.ToInt32(Console.ReadLine());
-            goldLeft = (golds % (crystals* crystalForGold));
+            goldLeft = (golds - (crystals* crystalForGold));
             Console.Write("У вас " + (crystals) + " кристалов и " + (goldLeft) +" золота." );
             Console.ReadLine();
         }
