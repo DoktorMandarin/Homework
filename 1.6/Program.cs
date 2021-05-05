6561287dedf04e8d48fe887e0e4fd8c71c080ed8
DoktorMandarin <faa1995alex@gmail.com>
2021-05-05 21:21:33 +0300

    задача 1.6

---
+++ 1.6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string youName;
+            string youLiveSity;
+            int youschool;
+            int youAge;
+
+            Console.Write("Как вас зовут?");
+            youName = Console.ReadLine();
+            Console.Write("Сколько вам лет?");
+            youAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("В каком городе вы живете?");
+            youLiveSity = Console.ReadLine();
+            Console.Write("Какую школу вы закончили?");
+            youschool = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вас зовут " + (youName) + ". Вам "  + (youAge) + " лет. Вы живете в " + (youLiveSity) + ". Вы закончили школу №" + (youschool) + "." );
+            
+
+        }
+    }
+}
