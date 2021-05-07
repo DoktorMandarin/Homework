6b14855975b349b8c986098ec77a39aa37a2665a
DoktorMandarin <faa1995alex@gmail.com>
2021-05-07 19:12:36 +0300

    задача 1.1

---
+++ 1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfLives = 1;
+            int yourGold = 5000;
+            int neededGold = 3000;
+            char '&';
+            string = "true";
+            bool = false;
+            int moreAttempts = 999;
+            int coinsLeft = 0;
+            int doggy = 7;
+            int gameInstalled = 3;
+
+        }
+    }
+}
