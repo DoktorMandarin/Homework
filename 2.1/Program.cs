d3b5c7682ecf9bf243789231129cf514882a8669
DoktorMandarin <faa1995alex@gmail.com>
2021-05-15 23:41:05 +0300

    задача 2.1

---
+++ 2.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int attempts;
+            Console.Write("Сколько раз вы хотите увидеть слово Привет!!! ?");
+            attempts = Convert.ToInt32(Console.ReadLine());
+
+            while (attempts -- > 0)
+            {
+                Console.WriteLine("Привет!!!");
+                
+            }
+            Console.ReadLine();
+
+        }
+        
+    }
+    
+}
+
