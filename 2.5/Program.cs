3e9e160d059b0b6f9f062dddea6e37341aa81041
DoktorMandarin <faa1995alex@gmail.com>
2021-07-21 18:54:23 +0300

    Задача 2.5

---
+++ 2.5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sequence = 0;
+
+            for (int i = 0; i < 14; i ++)
+            {
+                sequence += 7; 
+                Console.WriteLine(sequence);
+            }
+        }
+    }
+}
