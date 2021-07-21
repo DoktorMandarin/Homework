b4553e922fc762b2a299552c550d142364259407
DoktorMandarin <faa1995alex@gmail.com>
2021-07-21 18:54:37 +0300

    задача 2.7

---
+++ 2.7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 10;
+            string password = "Кукушка";
+            string userInput;
+
+            for (int i = 0; i < tryCount; i++) 
+            {
+                Console.Write("Введите пароль:");
+                userInput = Console.ReadLine();
+
+                if(password == userInput)
+                {
+                    Console.Write("Верно!!!");
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("У вас осталось - " +  (tryCount - i-1) + " попыток.");
+                }
+
+            }
+        }
+    }
+}
